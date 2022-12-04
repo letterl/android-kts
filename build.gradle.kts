8d572c36ca99cef495ca9ced6196b6e02e49c1f1
zhoulanzhen <zhoulanzhen>
2022-12-04 17:57:42 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    // 多个变量
+    rootProject.extra.apply {
+        set("compose_version", "1.1.1")
+        set("kotlin_version", "1.7.10")
+    }
+}
+
+plugins {
+    id("com.android.application").version( "7.3.1").apply(false)
+    id("com.android.library").version("7.3.1").apply( false)
+    id("org.jetbrains.kotlin.android").version("1.6.10").apply(false)
+}
